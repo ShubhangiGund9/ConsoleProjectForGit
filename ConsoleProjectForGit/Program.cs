3c39021da9f7f6493c849125105ed1737f31e47b
ShubhangiGund9 <shubhangigund15@gmail.com>
2025-06-30 15:09:19 +0530

    sdf

--- ConsoleProjectForGit/Program.cs
+++ ConsoleProjectForGit/Program.cs
@@ -12,8 +12,15 @@ namespace ConsoleProjectForGit
         {
             int a = 10, b = 20, c;
             c = a + b;
-            Console.WriteLine("Add=" + c)
+            Console.WriteLine("Add=" + c);
         }
+        public void Sub()
+        {
+            int a = 10, b = 20, c;
+            c = a - b;
+            Console.WriteLine("Sub=" , c1);
+        }
+
         static void Main(string[] args)
         {
             Program p = new Program();
